bda14f780d68a11d9bfd426ecd1e4f879c524968
walter.torricos <walticotc@gmail.com>
2019-06-30 10:42:07 -0400

    Remove unnecessary things from razor views.

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }
